dc48b82e88a9a9fb8ff881bbb4ea2db4181a284a
Kevin Li <kevyuecl@unc.edu>
2022-09-27 22:23:49 -0400

    utils and utils test progress

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,47 @@
+"""EX05- List Utility Functions (Implementation)."""
+__author__ = "730559522"
+
+
+def only_evens(int_list: list[int]) -> list[int]:
+    """Returns only even integers from the input list."""
+    i: int = 0
+    even_num_list: list[int] = []
+    while (i < len(int_list)):
+        if int_list[i] % 2 == 0:
+            even_num_list.append(int_list[i])
+            i += 1
+        else:
+            i += 1
+    return even_num_list
+
+
+def concat(int_list_one: list[int], int_list_two: list[int]) -> list[int]:
+    """Returns list containing all elements of 1st list followed ay all elements of 2nd list."""
+    list_concatenated: list[int] = []
+    first_index: int = 0
+    second_index: int = 0
+    while (first_index < len(int_list_one)):
+        list_concatenated.append(int_list_one[first_index])
+        first_index += 1
+    while (second_index < len(int_list_two)):
+        list_concatenated.append(int_list_two[second_index])
+        second_index += 1
+    return list_concatenated
+
+
+def sub(inputed_list: list[int], start_index: int, end_index: int) -> list[int]:
+    """Returns list that is subset of given list, between start index and end index - 1."""
+    subset_list: list[int] = []
+    if len(inputed_list) == 0 and start_index > len(inputed_list) or end_index <= 0:
+        return subset_list
+    elif end_index > len(inputed_list):
+        end_index = len(inputed_list)
+    elif start_index < 0:
+        start_index = 0
+    while (start_index < end_index):
+        subset_list.append(inputed_list[start_index])
+        start_index += 1
+    return subset_list
+
+
+
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,75 @@
+"""EX05- List Utility Functions (tests)."""
+__author__ = "730559522"
+
+
+from utils import only_evens, sub, concat
+
+
+def test_only_evens_no_item() -> None:
+    """Tests list with no items."""
+    int_list: list[int] = []
+    assert only_evens(int_list) == []
+
+
+def test_only_evens_first_integer_combination() -> None:
+    """Tests list with a combination of even and odd integers."""
+    int_list: list[int] = [1, 2, 4, 5]
+    assert only_evens(int_list) == [2, 4]
+
+
+def test_only_evens_second_integer_combination() -> None:
+    """Tests list with another combination of even and odd integers."""
+    int_list: list[int] = [3, 6, 8, 11]
+    assert only_evens(int_list) == [6, 8]
+
+
+def test_concat_no_item() -> None:
+    """Tests list with no items."""
+    int_list_one: list[int] = []
+    int_list_two: list[int] = []
+    assert concat(int_list_one, int_list_two) == []
+
+
+def test_concat_use1() -> None:
+    """Tests list use cases of the same length."""
+    int_list_one: list[int] = [1, 3, 5]
+    int_list_two: list[int] = [2, 4, 6]
+    assert concat(int_list_one, int_list_two) == [1, 3, 5, 2, 4, 6]
+
+
+def test_concat_use2() -> None:
+    """Tests list use cases of different lengths."""
+    int_list_one: list[int] = [1, 3, 5]
+    int_list_two: list[int] = [7, 9]
+    assert concat(int_list_one, int_list_two) == [1, 3, 5, 7, 9]
+
+
+def test_sub_no_item() -> None:
+    """Tests list with no items."""
+    inputed_list: list[int] = []
+    start_index = 0
+    end_index = 3
+    assert sub(inputed_list, start_index, end_index) == []
+
+
+def test_sub_use1() -> None:
+    """Tests list that is expected based on start and end indices."""
+    inputed_list: list[int] = [3, 4, 5, 6]
+    start_index = 0
+    end_index = 2
+    assert sub(inputed_list, start_index, end_index) == [3, 4]
+
+
+def test_sub_use2() -> None:
+    """Test list that is expected based on start and end indices for another set of integers."""
+    inputed_list: list[int] = [5, 7, 3, 6, 1]
+    start_index = 1
+    end_index = 3
+    assert sub(inputed_list, start_index, end_index) == [7, 3]
+
+
+
+
+
+
+
